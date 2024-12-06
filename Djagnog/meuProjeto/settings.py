638a902a48c9cb32d3474055d323fd7fcc224c13
Bruno Oliveira Carvalho <160294717+BrunoHznd@users.noreply.github.com>
2024-12-06 13:29:55 -0300

    Update settings.py

--- Djagnog/meuProjeto/settings.py
+++ Djagnog/meuProjeto/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-fyd)p6xintf!4@cmpp)yjzn%ob51ke3dv8_$&sy3un4&oef$ak
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True # mudar
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
